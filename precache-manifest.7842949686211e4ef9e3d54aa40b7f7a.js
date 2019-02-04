39ffe37230e4db8822c2c6270a568224d172eb1c
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-02-04 17:29:30 +0100

    Updates

---
+++ precache-manifest.7842949686211e4ef9e3d54aa40b7f7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49aa2f4f37f543de265b",
+    "url": "/ckeditor-playground/static/js/runtime~main.49aa2f4f.js"
+  },
+  {
+    "revision": "a7a7d810d10f0b94263a",
+    "url": "/ckeditor-playground/static/js/main.a7a7d810.chunk.js"
+  },
+  {
+    "revision": "a9756dfc86e806d5cb20",
+    "url": "/ckeditor-playground/static/js/1.a9756dfc.chunk.js"
+  },
+  {
+    "revision": "a7a7d810d10f0b94263a",
+    "url": "/ckeditor-playground/static/css/main.eecd38cd.chunk.css"
+  },
+  {
+    "revision": "a9756dfc86e806d5cb20",
+    "url": "/ckeditor-playground/static/css/1.e28d668c.chunk.css"
+  },
+  {
+    "revision": "170631feaacdb9c10c6965d591b6e616",
+    "url": "/ckeditor-playground/index.html"
+  }
+];
