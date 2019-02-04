cb2f320be0bd8612cb127b9e2e4007aaa1f7f461
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-02-04 17:24:18 +0100

    working document editor, added a bit of styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from "react";
 import CKEditor from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import DecoupledEditor from "@ckeditor/ckeditor5-build-decoupled-document";
+import theme from "@ckeditor/ckeditor5-theme-lark";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Using CKEditor 5 build in React</h2>
+        <h2>CKEditor 5 using a custom build - DecoupledEditor</h2>
         <CKEditor
-          editor={ClassicEditor}
-          data="<p>Hello from CKEditor 5!</p>"
           onInit={editor => {
-            // You can store the "editor" and use when it is needed.
             console.log("Editor is ready to use!", editor);
+            // Insert the toolbar before the editable area.
+            editor.ui.view.editable.element.parentElement.insertBefore(
+              editor.ui.view.toolbar.element,
+              editor.ui.view.editable.element
+            );
           }}
           onChange={(event, editor) => {
             const data = editor.getData();
-            console.log({ event, editor, data });
+            console.log(data);
           }}
-          onBlur={editor => {
-            console.log("Blur.", editor);
-          }}
-          onFocus={editor => {
-            console.log("Focus.", editor);
+          editor={DecoupledEditor}
+          config={{
+            theme: theme
           }}
         />
       </div>
